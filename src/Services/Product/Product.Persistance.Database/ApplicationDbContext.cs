9fdfc65f1cda49ec4499bffeefee09948c10e9ce
Fernando Bascope <fbascope@info-arch.com>
2023-03-27 16:37:54 -0400

    Add project files.

---
+++ src/Services/Product/Product.Persistance.Database/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Product.Domain;
+using Microsoft.EntityFrameworkCore;
+using Product.Persistance.Database.Configuration;
+
+namespace Product.Persistance.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Domain.Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.HasDefaultSchema("Product");
+
+            ModelConfig(modelBuilder);
+        }
+
+        private static void ModelConfig(ModelBuilder modelBuilder)
+        {
+            new ProductConfiguration(modelBuilder.Entity<Domain.Product>());            
+        }
+    }
+}
---
+++ src/Services/Product/Product.Persistance.Database/Configuration/ProductConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Product.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product.Persistance.Database.Configuration
+{
+    public class ProductConfiguration
+    {
+        public ProductConfiguration(EntityTypeBuilder<Domain.Product> entityTypeBuilder)
+        {
+            entityTypeBuilder.HasKey(x => x.ProductId);
+            entityTypeBuilder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+            entityTypeBuilder.Property(x => x.Description).IsRequired().HasMaxLength(500);
+        }
+    }
+}
